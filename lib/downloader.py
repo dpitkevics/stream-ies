d7da16649dde88e648f396e24881896e3c6b72dc
Daniels Pitkevičs <daniels.pitkevics@gmail.com>
2015-02-17 22:02:17 +0200

    sarakstīta kind of dokumentācija

--- lib/downloader.py
+++ lib/downloader.py
@@ -9,6 +9,10 @@ import threading
 
 
 class Download:
+    """
+    Download class for downloading torrent files
+    """
+
     torrent_name = None
     video_player = None
 
@@ -17,10 +21,24 @@ class Download:
     info = None
 
     def __init__(self, torrent_name, video_player=None):
+        """
+        Init method for Download class - sets local class variables passed in __init__
+
+        :param torrent_name: string
+        :param video_player: StreamIesVideoPlayer
+        :return:
+        """
+
         self.torrent_name = torrent_name
         self.video_player = video_player
 
     def set_handle(self):
+        """
+        Creates torrent session on specified ports.
+        Retrieves torrent info and passes next to torrent handle which is prioritized to download pieces in sequence
+        :return:
+        """
+
         ses = lt.session()
         ses.listen_on(6881, 6891)
 
@@ -34,9 +52,21 @@ class Download:
         self.handle = h
 
     def retrieve_torrent_info(self):
+        """
+        Returns information of torrent
+
+        :return: libtorrent.torrent_info
+        """
+
         return lt.torrent_info(self.torrent_name)
 
     def retrieve_video_file_path(self):
+        """
+        Searches torrent information for directory where all torrents will be downloaded
+
+        :return: string
+        """
+
         info = self.retrieve_torrent_info()
 
         dir_name = ''
@@ -53,6 +83,13 @@ class Download:
         return dir_name
 
     def retrieve_video_file_name(self):
+        """
+        Retrieves video file name which is saved in local pc
+        Searching is through specified directory moving through all the files
+
+        :return: string
+        """
+
         torrent_dir = self.retrieve_video_file_path()
 
         current_torrent_dir = DOWNLOAD_DIR + torrent_dir
@@ -71,9 +108,17 @@ class Download:
         return video_path
 
     def download_torrent(self):
+        """
+        Makes actual download of torrent files
+
+        :return:
+        """
+
+        # Sets handle if is needed
         if self.handle is None:
             self.set_handle()
 
+        # Looping while handle is not seeding but downloading
         while not self.handle.is_seed():
             s = self.handle.status()
 
@@ -85,40 +130,77 @@ class Download:
                          'seeding',
                          'allocating',
                          'checking fastresume']
+
+            # Prints current torrent status
             print '\r%.2f%% complete (down: %.1f kb/s up: %.1f kB/s peers: %d) %s' % \
                   (s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000,
                    s.num_peers, state_str[s.state]),
             sys.stdout.flush()
 
             video_path = self.retrieve_video_file_name()
+            # Checking if we need to create .st file
             if not os.path.isfile(video_path + ".st"):
+                # Checking if we are able to create .st file
                 if (s.progress * 100) > 1:
                     open(video_path + ".st", 'a').close()
 
+                    # Setting new video data for our player
                     self.video_player.is_video_download_started = True
                     self.video_player.video_path = video_path
                     self.video_player.set_video_source(video_path)
 
             time.sleep(1)
 
+        # Download is finished, set new video data for our player and make a .dn file
         self.video_player.is_video_download_done = True
         video_path = self.retrieve_video_file_name()
         open(video_path + ".dn", 'a').close()
 
 
 class ThreadedDownload(threading.Thread, Download):
+    """
+    Makes basic download to a threaded download so it will not freeze all program, but will run in background
+    """
+
     def __init__(self, torrent_name, video_player=None):
+        """
+        Initializes both parent classes as well as creating stop event to stop thread when needed
+
+        :param torrent_name: string
+        :param video_player: StreamIesVideoPlayer
+        :return:
+        """
+
         threading.Thread.__init__(self)
         Download.__init__(self, torrent_name, video_player)
         self._stop = threading.Event()
 
     def run(self):
+        """
+        Starts thread with downloading torrent
+
+        :return:
+        """
+
         self.download_torrent()
 
     def download_torrent(self):
+        """
+        Performs actual download by calling parent class download method
+        After download, stops thread
+
+        :return:
+        """
+
         super(ThreadedDownload, self).download_torrent()
 
         self.stop()
 
     def stop(self):
+        """
+        Stops current thread
+
+        :return:
+        """
+
         self._stop.set()
--- lib/video.py
+++ lib/video.py
@@ -3,19 +3,34 @@ from types import StringType
 from hachoir_core.error import HachoirError
 from hachoir_core.cmd_line import unicodeFilename
 from hachoir_parser import createParser
-from hachoir_core.tools import makePrintable
 from hachoir_metadata import extractMetadata
-from hachoir_core.i18n import getTerminalCharset
 
 
 class Metadata:
+    """
+    Class for retrieving metadata for video files
+    """
 
     video_path = StringType('')
 
     def __init__(self, video_path):
+        """
+        Initializes class. Sets video_path as class parameter
+
+        :param video_path: string
+        :return:
+        """
+
         self.video_path = video_path
 
     def extract_metadata(self):
+        """
+        Extracts metadata and returns it
+        If video fail is invalid, exception is raised
+
+        :return: list
+        """
+
         filename, realname = unicodeFilename(self.video_path), self.video_path
         parser = createParser(filename, realname)
         if not parser:
@@ -32,6 +47,4 @@ class Metadata:
 
         text = metadata.exportPlaintext()
 
-        charset = getTerminalCharset()
-        for line in text:
-            print makePrintable(line, charset)
+        return text
