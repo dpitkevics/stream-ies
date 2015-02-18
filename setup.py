b9d8ce0871f44ecce6a8d57d62f1e1ca6f6fb930
Daniels Pitkevičs <daniels.pitkevics@gmail.com>
2015-02-18 11:32:12 +0200

    added setup.py which is really freakin incomplete

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='stream-ies',
+    version='0.01',
+    packages=['lib'],
+    url='',
+    license='',
+    author='Daniels',
+    author_email='',
+    description='',
+    install_requires=[
+        'libtorrent',
+        'hachoir_core',
+        'hachoir_parser',
+        'hachoir_metadata',
+    ]
+)
