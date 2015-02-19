4468709611171bcee544e5cecb1ed1ccd602aa2b
Daniels Pitkevičs <daniels.pitkevics@gmail.com>
2015-02-19 22:48:40 +0200

    commented out code removed

--- adapters/tv_show_adapter.py
+++ adapters/tv_show_adapter.py
@@ -12,17 +12,9 @@ class TvShowAdapterButton(ListItemButton):
     def on_menu_selection(self, index):
         series_name = self.adapter.data[index]
 
-        # series_searcher = SeriesSearcher()
-        # data = series_searcher.search_show_data_by_id(self.adapter.series_ids[series_name])
-
         app = StreamIesApp.get_running_app()
         widget = app.interface_manager.retrieve_active_widget()
         widget.show_episodes_in_list_view(series_name)
-        # app.interface_manager.switch_form('tv_show')
-        #
-        # widget = app.interface_manager.retrieve_active_widget()
-        # widget.set_show_data(data)
-        # widget.update_list_view()
 
 
 class TvShowAdapter(ListAdapter):
